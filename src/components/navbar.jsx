328cf74f978ad5b980ad4c1e2c6c01d3970a5d43
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-02-13 15:18:29 +0100

    Did: active link

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import NavLink from "./navLink";
 
 const links = [
 	{ url: "/", title: "Home" },
@@ -14,17 +15,15 @@ const Navbar = () => {
 	const [isOpen, setIsOpen] = useState(false);
 
 	return (
-		<div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+		<div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
 			{/* LINKS */}
 			<div className="hidden md:flex gap-4 w-1/3">
 				{links.map((link) => (
-					<Link href={link.url} key={link.url}>
-						{link.title}
-					</Link>
+					<NavLink link={link} key={link.url} />
 				))}
 			</div>
 			{/* LOGO */}
-			<div className="md:hidden lg:flex w-1/3 justify-center">
+			<div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
 				<Link
 					href="/"
 					className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
