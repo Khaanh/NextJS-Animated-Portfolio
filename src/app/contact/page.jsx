89b35babaa1c82cc0b77ba5120060c964ef64c68
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-02-17 15:39:02 +0100

    Did: contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,41 @@
 "use client";
 import { motion } from "framer-motion";
+import { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactPage = () => {
+	const [success, setSuccess] = useState(true);
+	const [error, setError] = useState(false);
 	const text = "Say Hello";
+
+	const form = useRef();
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+		setError(false);
+		setSuccess(false);
+
+		emailjs
+			.sendForm(
+				process.env.NEXT_PUBLIC_SERVICE_ID,
+				process.env.NEXT_PUBLIC_TEMPLATE_ID,
+				form.current,
+				{
+					publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+				}
+			)
+			.then(
+				(result) => {
+					// console.log("SUCCESS!");
+					setSuccess(true);
+					form.current.reset();
+				},
+				(error) => {
+					setError(true);
+					form.current.reset();
+				}
+			);
+	};
 
 	return (
 		<motion.div
@@ -13,7 +46,7 @@ const ContactPage = () => {
 		>
 			<div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
 				{/* TEXT CONTAINER */}
-				<div className="h-1/2 lg:h-full lg:w-1/2">
+				<div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
 					<div>
 						{text.split("").map((letter, index) => (
 							<motion.span
@@ -33,7 +66,38 @@ const ContactPage = () => {
 					</div>
 				</div>
 				{/* TEXT CONTAINER */}
-				<div className="h-1/2 lg:h-full lg:w-1/2"></div>
+				<form
+					onSubmit={sendEmail}
+					ref={form}
+					className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24"
+				>
+					<span>Dear Khanh Nguyen,</span>
+					<textarea
+						rows={6}
+						className="bg-transparent border-b-2 border-b-black outline-none resize-none"
+						name="user_email"
+					/>
+					<span>My mail address is:</span>
+					<input
+						name="user_email"
+						type="text"
+						className="bg-transparent border-b-2 border-b-black outline-none"
+					/>
+					<span>Regards</span>
+					<button className="bg-purple-200 rounded font-semibold text-gray-600 p-4">
+						Send
+					</button>
+					{success && (
+						<span className="text-green-600 font-semibold text-base md:text-lg lg:text-xl">
+							Your message has been send successfully !
+						</span>
+					)}
+					{error && (
+						<span className="text-red-600 font-semibold">
+							Something went wrong !
+						</span>
+					)}
+				</form>
 			</div>
 		</motion.div>
 	);
